1e679d64a7aaca2a58f2d2a87012c79f39cf165d
fdsc <fdsc@yandex.ru>
2021-05-18 13:26:06 +0300

    Небольшие изменения после проверки keccak.cs (базовый алгоритм keccak - раунды и ввод в губку, но не сама губка)

--- cryptoprime/keccak.cs
+++ cryptoprime/keccak.cs
@@ -12,18 +12,18 @@ namespace cryptoprime
 {
     /// <summary>Статический класс, предоставляющий базовые функции и константы keccak. Пример использования см. в vinkekfish.Keccak_PRNG_20201128</summary>
     public static class keccak
-    {
-        public const int S_len  = 5;                // Длина строки матрицы в ulong
-        public const int S_len2 = S_len*S_len;      // Размер матрицы в ulong (25*ulong)
+    {                                                       /// <summary>Длина строки матрицы в ulong: 5 элементов ulong. См. также c_size</summary>
+        public const int S_len  = 5;                        /// <summary>Размер матрицы в значениях ulong: 5*5=25 элементов ulong. См. также b_size</summary>
+        public const int S_len2 = S_len*S_len;
+                                                            /// <summary>rate в битах - 576 (размер внешней части криптографического состояния - блок вводимых данных)</summary>
+        public const int r_512  = 576;                      /// <summary>rate в байтах = 72, это размер блока ввода/вывода за один раз</summary>
+        public const int r_512b = 72;                       /// <summary>rate в ulong = 9</summary>
+        public const int r_512s = 9;
+                                                            /// <summary>25*8=200. Размер основной матрицы S (или "a") и вспомогательной матрицы b в байтах - равен размеру криптографического состояния</summary>
+        public const int b_size = 25*8;                     /// <summary>5*8=40. Размер вспомогательной матрицы c в байтах - равен размеру строки криптографического состояния. Используется для транспонирования</summary>
+        public const int c_size =  5*8;
 
-        public const int   r_512  = 576;
-        public const int   r_512b = r_512 >> 3; // 72
-        public const int   r_512s = r_512 >> 6; // 9
-
-        public const int   b_size = 25*8;           // Размер вспомогательной матрицы b в байтах - равен размеру криптографического состояния
-        public const int   c_size =  5*8;           // Размер вспомогательной матрицы c в байтах - равен размеру строки криптографического состояния
-
-        public static readonly int[]   rNumbers = {1152, 1088, 832, 576}; // 224, 256, 384, 512 битов
+        /// <summary>Раундовые коэффициенты для шага ι</summary>
         public static readonly ulong[] RC =
         {
             0x0000000000000001,
@@ -58,8 +58,10 @@ namespace cryptoprime
             0x8000000080008008
         };
 
-        // Реализация раундов keccak и раундового преобразования
-        // Раундовое преобразование
+        /// <summary>Реализация раундового преобразования</summary>
+        /// <param name="a">Матрица S (a) - внутреннее состояние. Размер b_size (25*8=200 байтов) либо S_len2 в ulong (25 ulong)</param>
+        /// <param name="c">Вспомогательная матрица размером c_size (5*8=40 байтов)</param>
+        /// <param name="b">Вспомогательная матрица размером b_size (25*8=200 байтов)</param>
         public static unsafe void roundB(ulong * a, ulong * c, ulong * b)
         {
             //шаг θ
@@ -173,10 +175,10 @@ namespace cryptoprime
         }
 
         // Полный keccak
-        /// <summary>Все раунды keccak. a == S, c= C, b = B</summary>
-        /// <param name="a">Зафиксированное внутреннее состояние S: 25 * ulong (константа b_size или S_len2*8)</param>
-        /// <param name="c">Массив C (значения не важны):   5 * ulong (константа c_size)</param>
-        /// <param name="b">Матрица B (значения не важны): 25 * ulong (константа b_size)</param>
+        /// <summary>Все раунды keccak (24 раунда). a == S, c= C, b = B</summary>
+        /// <param name="a">Зафиксированное внутреннее состояние S: 25 * ulong (константа b_size или S_len2*ulong)</param>
+        /// <param name="c">Массив  C (значения не важны):  5 * ulong (константа c_size=40)</param>
+        /// <param name="b">Матрица B (значения не важны): 25 * ulong (константа b_size=200)</param>
         public static unsafe void Keccackf(ulong * a, ulong * c, ulong * b)
         {
             roundB(a, c, b);
@@ -213,12 +215,11 @@ namespace cryptoprime
             roundB(a, c, b); *a ^= 0x8000000080008008;
         }
 
-        // keccak с неполными раундами
-        /// <summary>Неполнораундовый keccack</summary>
+        /// <summary>Неполнораундовый keccack. Параметры аналогичны Keccackf</summary>
         /// <param name="a">Внутреннее состояние S</param>
         /// <param name="c">Массив C (состояние не важно)</param>
         /// <param name="b">Матрица B (состояние не важно)</param>
-        /// <param name="start">Начальный шаг, от нуля</param>
+        /// <param name="start">Начальный раунд, считается от нуля</param>
         /// <param name="count">Количество шагов (всего шагов столько, сколько констант в RC)</param>
         public static unsafe void Keccack_i(ulong * a, ulong * c, ulong * b, int start, int count)
         {
@@ -239,7 +240,7 @@ namespace cryptoprime
         {
             if (len > r_512b || len < 0)
             {
-                throw new ArgumentOutOfRangeException("len > r_512b || len < 0");
+                throw new ArgumentOutOfRangeException("cryptoprime.keccak.Keccak_Input_512: len > r_512b || len < 0");
             }
 
             // В конце 72-хбайтового блока нужно поставить оконечный padding
@@ -251,8 +252,8 @@ namespace cryptoprime
             byte * es    = S + 135;
             byte * lastS = S;           // Если len = 0, то записываем в первый байт
             // Общий смысл инициализации
-            // Массив информации в размере 72 байта записывается в начало состояния из 25-ти 8-мибайтовых слов; однако матрица S при этом имеет транспонированные индексы
-            int i1 = 0, i2 = 0, i3 = 0, ss = S_len << 3;
+            // Массив информации в размере 72 байта записывается в начало состояния из 25-ти 8-мибайтовых слов; однако матрица S при этом имеет транспонированные индексы для повышения криптостойкости. То есть запись идёт не в начало матрицы S, а в начало транспонированной матрицы S
+            int i1 = 0, i2 = 0, i3 = 0, ss = c_size;
             for (int i = 0; i < len; i++)
             {
                 lastS = S + (i1 << 3) + i2*ss + i3;   // i2*ss - не ошибка, т.к. индексы в матрице транспонированны
@@ -268,15 +269,13 @@ namespace cryptoprime
                 }
                 if (i2 >= S_len)
                 {
-                    i2 = S_len;
                     i2 = 0;
                     i1++;
                 }
 
-                // Это даже на последней итерации не должно выполняться
-                if (i1 >= S_len)
+                if (i1 > 1)
                 {
-                    throw new Exception();
+                    throw new Exception("cryptoprime.keccak.Keccak_Input_512: Fatal algorithmic error");
                 }
 
                 // Это вычисление нужно для того, чтобы потом записать верно padding
@@ -286,8 +285,11 @@ namespace cryptoprime
 
             if (setPaddings)
             {
-                if (len >= r_512b)
-                    throw new ArgumentOutOfRangeException("len >= r_512b (must be < 72)");
+                var SE = S + b_size;
+
+                // На всякий случай, проверка на выход за пределы массива
+                if (lastS >= SE || es >= SE)
+                    throw new ArgumentOutOfRangeException("cryptoprime.keccak.Keccak_Input_512: lastS >= SE || es >= SE");
 
                  *lastS ^= 0x01;
                  *es    ^= 0x80;
@@ -310,7 +312,7 @@ namespace cryptoprime
             const byte RB = 64;
             if (len > RB || len < 0)
             {
-                throw new ArgumentOutOfRangeException("len > 64 || len < 0");
+                throw new ArgumentOutOfRangeException("cryptoprime.keccak.Keccak_InputOverwrite64_512: len > 64 || len < 0");
             }
 
             byte * lastS  = S;           // Если len = 0, то записываем в первый байт
@@ -318,10 +320,10 @@ namespace cryptoprime
             // Общий смысл инициализации
             // Массив информации в размере 64 байта записывается в начало состояния из 25-ти 8-мибайтовых слов; однако матрица S при этом имеет транспонированные индексы
             // Байты, которые не введены, просто считаются нулями (то есть состояние всё равно перезаписывается)
-            // Никаких paddings не делаем!
-            int i1 = 0, i2 = 0, i3 = 0, ss = S_len << 3;
+            // Дописываем len после конца: это не paddings, это отличие от оригинальной версии keccak
+            int i1 = 0, i2 = 0, i3 = 0, ss = c_size;
             int i = 0;
-            for (; i < RB; i++) // +1 чтобы сделать padding
+            for (; i < RB; i++)
             {
                 lastS = S + (i1 << 3) + i2*ss + i3;
                 if (i < len)
@@ -345,19 +347,27 @@ namespace cryptoprime
                     i1++;
                 }
 
-                // Это даже на последней итерации не должно выполняться
-                if (i1 >= S_len)
+                if (i1 > 1)
                 {
-                    throw new Exception();
+                    throw new Exception("cryptoprime.keccak.Keccak_InputOverwrite64_512: Fatal algorithmic error");
                 }
             }
+
+            // Дописываем после конца ввода размер введённых данных
+            lastS = S + (i1 << 3) + i2*ss + i3;
+
+            // На всякий случай, проверка на выход за пределы границ матрицы S
+            if (lastS >= S + b_size)
+                throw new ArgumentOutOfRangeException("cryptoprime.keccak.Keccak_InputOverwrite64_512: lastS >= S + b_size");
+
+            *lastS ^= len;
         }
 
         /// <summary>Вывод данных из состояния keccak. Предназначен только для версии 512 битов</summary>
-        /// <param name="output">Указатель на массив, готовый принять данные</param>
+        /// <param name="output">Указатель на массив, готовый принять данные :[0, 72]</param>
         /// <param name="len">Количество байтов для записи (не более 72-х; константа r_512b). Обычно используется 64 - это стойкость данного криптографического преобразования</param>
-        /// <param name="S">Внутреннее состояние S</param>
-        // При вызове надо проверить, что output всегда нужной длины
+        /// <param name="S">Внутреннее состояние S :[200]</param>
+        /// <remarks>При вызове надо проверить, что output всегда нужной длины</remarks>
         public static unsafe void Keccak_Output_512(byte * output, byte len, byte * S)
         {
             if (len > r_512b || len < 0)
